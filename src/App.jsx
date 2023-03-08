7f1a4512a7319d1f0f3cd0fb55d0d576530f67db
Sagul-09 <nihal.evil.royal@gmail.com>
2023-03-08 22:00:12 +0530

    added icons and hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
-import { TbBrandVscode } from "react-icons/tb";
+import { TbBrandVscode, TbBrandJavascript } from "react-icons/tb";
 import { SiAdobepremierepro, SiCanva } from "react-icons/si";
 import { RxFigmaLogo } from "react-icons/rx";
+import { GrLanguage, GrWorkshop, GrProjects, GrDomain } from "react-icons/gr";
+import { BsTools } from "react-icons/bs";
+import { GiSkills } from "react-icons/gi";
+import { BiBook, BiRename } from "react-icons/bi";
+import { BsPersonVcard, BsPersonAdd } from "react-icons/bs";
+import { FaLinkedin, FaGithub, FaInstagram, FaHtml5, FaCss3Alt, FaPython } from "react-icons/fa";
+import { CgPacman } from "react-icons/cg";
+
 
 function App() {
   return (
@@ -12,14 +20,23 @@ function App() {
             <img src="src/img/pro.jpeg" className="pic"></img>
           </div>
           <div className="info">
-            <h1 className="name">SAGUL</h1>
-            <h1 className="domain">FRONT-END DEVELOPER</h1>
+            <h1 className="name">
+              <BiRename />:<span> SAGUL</span>
+            </h1>
+            <h1 className="domain">
+              <span>
+                <GrDomain />
+              </span>
+              : FRONT-END DEVELOPER
+            </h1>
           </div>
         </div>
         <hr></hr>
         {/* about */}
         <div className="container1">
-          <h4 className="title">About</h4>
+          <h4 className="title">
+            About <BsPersonVcard />:
+          </h4>
           <div className="content">
             As a front-end web developer, I specialize in the design and
             implementation of user-facing web pages and applications. I have a
@@ -29,7 +46,9 @@ function App() {
         {/* QUALIFICATION BOX CONTAINER1*/}
         <div className="container1">
           {/* <div className="content"> */}
-          <h4 className="title">QUALIFICATION:</h4>
+          <h4 className="title">
+            QUALIFICATION <BiBook />:
+          </h4>
           <div className="content">
             <table className="table">
               <tr>
@@ -57,39 +76,57 @@ function App() {
         </div>
         {/* SKILL  */}
         <div className="container1">
-          <h4 className="title">SKILLS:</h4>
+          <h4 className="title">
+            SKILL'S <GiSkills />:
+          </h4>
           <div className="content">
             <ul>
-              <li>C</li>
+              <li>
+                C <CgPacman />
+              </li>
               <div className="progress-bar">
-                <div className="c">
-                  <span>60%</span>
-                </div>
+                <div className="c">{/* <span>60%</span> */}</div>
               </div>
-              <li>HTML</li>
+              <li>
+                HTML <FaHtml5 />
+              </li>
               <div className="progress-bar">
-                <div className="html">
-                  <span>75%</span>
-                </div>
+                <div className="html">{/* <span>75%</span> */}</div>
               </div>
-              <li>CSS</li>
+              <li>
+                CSS
+                <FaCss3Alt />
+              </li>
               <div className="progress-bar">
-                <div className="css">
-                  <span>65%</span>
-                </div>
+                <div className="css">{/* <span>65%</span> */}</div>
               </div>
-              <li>Python</li>
+
+              <li>
+                Js <TbBrandJavascript />
+              </li>
               <div className="progress-bar">
-                <div className="python">
-                  <span>45%</span>
-                </div>
+                <div className="js">{/* <span>45%</span> */}</div>
+              </div>
+
+              <li>
+                Python <FaPython />
+              </li>
+              <div className="progress-bar">
+                <div className="python">{/* <span>45%</span> */}</div>
               </div>
             </ul>
+            <div className="content">
+              <span className="bar-info">
+                The above bar represents my skill level{" "}
+              </span>
+            </div>
           </div>
         </div>
         {/* TOOLS */}
         <div className="container1">
-          <h4 className="title">TOOLS:</h4>
+          <h4 className="title">
+            TOOL'S <BsTools />:
+          </h4>
           <div className="content">
             <ul>
               <li>
@@ -113,7 +150,9 @@ function App() {
         </div>
         {/* certification */}
         <div className="container1">
-          <h4 className="title">WORKSHOP & CERTIFICATION:</h4>
+          <h4 className="title">
+            WORKSHOP & CERTIFICATION <GrWorkshop />:
+          </h4>
           <div className="content">
             <ul>
               <li>React-js</li>
@@ -123,15 +162,20 @@ function App() {
           </div>
         </div>
         <div className="container1">
-          <h4 className="title">PROJECTS:</h4>
+          <h4 className="title">
+            PROJECTS <GrProjects />:
+          </h4>
           <div className="content">
             <ul>
               <li>Resume using HTML & CSS</li>
+              <li>Simple lane detection system </li>
             </ul>
           </div>
         </div>
         <div className="container1">
-          <h4 className="title">LANGUAGE'S KNOWN :</h4>
+          <h4 className="title">
+            LANGUAGE'S KNOWN <GrLanguage />:
+          </h4>
           <ul>
             <li>English</li>
             <li>Tamil</li>
@@ -141,11 +185,19 @@ function App() {
         </div>
 
         <div className="container1">
-          <h4 className="title">SOCIAL:</h4>
+          <h4 className="title">
+            SOCIAL'S <BsPersonAdd />:
+          </h4>
           <ul>
-            <li>LinkedIn</li>
-            <li>Git-hub</li>
-            <li>Instagram</li>
+            <li>
+              LinkedIn <FaLinkedin /> <span className="id"> @sagul </span>
+            </li>
+            <li>
+              Git-hub <FaGithub /> <span className="id"> @sagul-09 </span>
+            </li>
+            <li>
+              Instagram <FaInstagram /> <span className="id"> @detail_snap_ </span>
+            </li>
           </ul>
         </div>
       </div>
